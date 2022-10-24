1125d6da5100b54fd2b8698f80355f4f0d8bd03f
Jeongyoon Song <sjyjudy710@naver.com>
2022-10-24 17:43:44 +0900

    엔티티 매핑1

--- JPA/ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ JPA/ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -1,21 +1,35 @@
 package jpabook.start;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name="MEMBER")
+@Table(name="MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"} )})
 public class Member {
 
     @Id
     @Column(name="ID")
     private String id;          // ID
-    @Column(name="NAME")
+
+    @Column(name="NAME", nullable = false, length = 10)
     private String username;    // NAME
+
     @Column(name="AGE")
     private Integer age;        // AGE
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;  // UWER, ADMIN 구분
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;   // 가입일
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;  // 수정일
+
+    @Lob
+    private String description; // 회원 설명
 
     public String getId() {
         return id;
@@ -39,5 +53,41 @@ public class Member {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public enum RoleType {
+        ADMIN, USER
     }
 }
