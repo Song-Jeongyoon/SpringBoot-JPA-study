0e805b0101bea927bb02b5adbf3f10f5c87c1ceb
Jeongyoon Song <sjyjudy710@naver.com>
2022-11-10 02:12:59 +0900

    예외처리

---
+++ Spring Boot/valid_exception/src/main/java/com/springboot/valid_exception/common/exception/CustomExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.springboot.valid_exception.common.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+// 컨트롤러에서 발생한 예외를 한 곳에서 처리할 수 있게
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    private Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class);
+
+    // 컨트롤러가 정의된 빈의 예외를 처리하는 메소드에 붙임
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleException(RuntimeException e, HttpServletRequest request) {
+
+        // 오류 발생을 알리는 응답 메시지 출력
+        HttpHeaders responseHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.error("Advice 내 handleException 호출, {}, {}", request.getRequestURI(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", e.getMessage());
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+    }
+}
---
+++ Spring Boot/valid_exception/src/main/java/com/springboot/valid_exception/controller/ExceptionController.java
@@ -0,0 +1,48 @@
+package com.springboot.valid_exception.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/exception")
+public class ExceptionController {
+
+    private Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
+
+    /* RuntimeException을 발생 */
+    @GetMapping
+    public void getRuntimeException() {
+
+        // RuntimeException 발생 코드
+        throw new RuntimeException("getRuntimeException 메소드 호출");
+    }
+
+    /* 해당 클래스에 국한해서 예외 처리 */
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleException(RuntimeException e, HttpServletRequest request) {
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.error("class 내 handleException 호출, {}, {}", request.getRequestURI(), e.getMessage());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error type", httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message", e.getMessage());
+
+        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+    }
+}
+
